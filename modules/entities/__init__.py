e600c789d145cde9d243990684d03fd55ac8ac50
lemm17 <lemmi7@yandex.ru>
2019-10-22 16:41:41 +0500

    Обновил презентацию

--- modules/entities/__init__.py
+++ modules/entities/__init__.py
@@ -1,22 +1,28 @@
 from flask import Flask
 from flask import render_template
+
 app = Flask("__name__")
+
 
 @app.route("/")
 def hello_world():
-    return "МЯСО СОСАТБ"
+    return ""
+
 
 @app.route("/profile")
 def profile():
     return render_template("profile.html")
 
-@app.route("/login") # Страница для логина, хотя особо смысла в ней не будет
+
+@app.route("/login")  # Страница для авторизации
 def login():
     pass
 
+
 @app.errorhandler(404)
 def page_not_found(error):
-    return "Нет такой странички"
+    return "<b>Страница не найдена</b><br>ERROR 404"
+
 
 if __name__ == "__main__":
     app.config["EXPLAIN_TEMPLATE_LOADING"] = True
