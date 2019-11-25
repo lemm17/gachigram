4a2af442a2aa548a2ac8098846773f97f589eb4b
Ilya <ilyaturtass@yandex.ru>
2019-11-25 18:08:45 +0500

    documented

--- app/forms.py
+++ app/forms.py
@@ -5,6 +5,16 @@ from app.entities import User
 
 
 class LoginForm(FlaskForm):
+    
+    '''Класс наследуется от FlaskForm и содержит поля формы входа пользователя
+
+    Четыре класса, которые представляют типы полей, которые я использую для этой 
+    формы, импортируются непосредственно из пакета WTForms, поскольку расширение 
+    Flask-WTF не предоставляет настраиваемые версии. Для каждого поля объект 
+    создается как переменная класса в классе LoginForm. Каждому полю присваивается 
+    описание или метка в качестве первого аргумента.
+    '''
+
     login = StringField('Login', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
@@ -12,6 +22,16 @@ class LoginForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
+   
+    '''Класс наследуется от FlaskForm и содержит поля формы регистрации пользователя
+
+    Пять классов, которые представляют типы полей, которые я использую для этой 
+    формы, импортируются непосредственно из пакета WTForms, поскольку расширение 
+    Flask-WTF не предоставляет настраиваемые версии. Для каждого поля объект 
+    создается как переменная класса в классе LoginForm. Каждому полю присваивается 
+    описание или метка в качестве первого аргумента.
+    '''
+
     login = StringField('Login', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -20,11 +40,21 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Register')
 
     def validate_username(self, username):
+        '''Метод класса RegistrationForm(FlaskForm)
+
+        Метод возвращает ошибку ValidationError() в случае, если логин, введённый
+        пользователем not None, то есть логин уже содержится в базе данных.
+        '''
         user = User.query.filter_by(login=username.data).first()
         if user is not None:
             raise ValidationError('Please use a different username.')
 
     def validate_email(self, email):
+        '''Метод класса RegistrationForm(FlaskForm)
+
+        Метод возвращает ошибку ValidationError() в случае, если email, введённый
+        пользователем not None, то есть email уже содержится в базе данных.
+        '''
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
